69e91dd0873bbdb881871231381ec7a4f2f2da30
Ilja Gusiatin <ilja.gusiatin@softeq.com>
2023-06-21 16:35:30 +0300

    feat: handle exit from File Manager

--- src/FileManager.js
+++ src/FileManager.js
@@ -1,13 +1,25 @@
+import { createInterface } from "readline/promises";
+import { EOL } from "os";
+
 export default class FileManager {
   constructor(username) {
     this.username = username || "Anonymous";
+    this.rl = createInterface(process.stdin);
   }
 
   start() {
     console.log(`Welcome to the File Manager, ${this.username}!`);
 
+    this.rl.on("line", (line) => {
+      console.log(`My line: ${line}`);
+    });
+
     process.on('exit', () => {
-      console.log(`Thank you for using File Manager, ${this.username}, goodbye!`);
-    })
+      console.log(`${EOL}Thank you for using File Manager, ${this.username}, goodbye!`);
+    });
+
+    process.on('SIGINT', () => {
+      process.exit();
+    });
   }
 }
